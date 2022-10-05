c92352b93a79dd8723a4323a0a4cf9c39df3a2ef
Yanko Rashkov <thewhitedead@abv.bg>
2022-10-05 19:36:23 +0300

    Create 07rounding.py

---
+++ 14 Functions - Lab/07rounding.py
@@ -0,0 +1,33 @@
+input_numbers = list(map(float, input().split()))
+
+print([round(number) for number in input_numbers])
+
+
+
+#################################### TASK CONDITION ############################
+"""
+                      7.	Rounding
+Write a program that rounds all the given numbers,
+separated by a single space, and prints the result as a list. Use round().
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+1.0 2.5 3.0 4.5
+
+Output
+[1, 2, 3, 4]
+
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+2.56 1.9 -3.4 8.1
+
+Output
+[3, 2, -3, 8]
+
+"""
