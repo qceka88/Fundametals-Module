6ddb746acb37f8a72c6ff306563413f99663cafd
Yanko Rashkov <thewhitedead@abv.bg>
2022-10-07 20:37:25 +0300

    Create 07min_max_and_sum.py

---
+++ 15 Function - Exercise/07min_max_and_sum.py
@@ -0,0 +1,45 @@
+numbers_list = list(map(int, input().split()))
+
+print(f"The minimum number is {min(numbers_list)}")
+print(f"The maximum number is {max(numbers_list)}")
+print(f"The sum number is: {sum(numbers_list)}")
+
+
+
+#################################### TASK CONDITION ############################
+"""
+7. Min Max and Sum
+Write a program that receives a sequence of numbers
+(integers) separated by a single space. It should print the min and max 
+values of the given numbers and the sum of all the numbers in the list. 
+Use min(), max() and sum().
+The output should be as follows:
+•	On the first line: "The minimum number is {minimum number}"
+•	On the second line: "The maximum number is {maximum number}"
+•	On the third line: "The sum number is: {sum of all numbers}"
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+2 4 6
+
+Output
+The minimum number is 2
+The maximum number is 6
+The sum number is: 12
+
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+12 52 11 53 2 8 45
+
+Output
+The minimum number is 2
+The maximum number is 53
+The sum number is: 183
+
+"""
