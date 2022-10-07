eff1edbed98d7119fc254c495a26a993ea2b47f9
Yanko Rashkov <thewhitedead@abv.bg>
2022-10-07 20:29:47 +0300

    Create 06sort.py

---
+++ 15 Function - Exercise/06sort.py
@@ -0,0 +1,34 @@
+list_of_numbers = list(map(int, input().split()))
+sorting = sorted(list_of_numbers)
+print(sorting)
+
+
+
+#################################### TASK CONDITION ############################
+"""
+                    6. Sort
+Write a program that receives a sequence of numbers (integers) 
+separated by a single space. It should print a sorted list 
+of numbers in ascending order. Use sorted().
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+6 2 4
+
+Output
+[2, 4, 6]
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+12 52 11 53 2 8 45
+
+Output
+[2, 8, 11, 12, 45, 52, 53]
+
+"""
