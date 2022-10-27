9b3be3ba3666c4f01231054a68d1617a63ec4390
Yanko Rashkov <thewhitedead@abv.bg>
2022-10-27 21:31:53 +0300

    Update 02party.py

--- 21-Objects and Classes - Lab/02party.py
+++ 21-Objects and Classes - Lab/02party.py
@@ -29,42 +29,50 @@ print(party.quantity_people())
 
 #################################### TASK CONDITION ############################
 '''
-                                  2.	Party
-                                  
-Create a class Party that only has an attribute people – empty list. 
-The __init__ method should not accept any parameters. 
-You will be given names of people (on separate lines) until you receive 
-the command "End". Use the created class to solve this problem. 
-
-After you receive the "End" command, print 2 lines:
-•	"Going: {people}" - the people should be separated by comma and space ", ".
-•	"Total: {total_people_going}"
-
+                          3.	Email
+Create class Email. The __init__ method should receive sender, 
+receiver and a content. It should also have a default set to 
+False attribute called is_sent. The class should have two additional methods:
+•	send() - sets the is_sent attribute to True
+•	get_info() - returns the following string:
+ "{sender} says to {receiver}: {content}. Sent: {is_sent}"
+You will receive some information (separated by a single space) 
+until you receive the command "Stop". The first element will be the sender, 
+the second one – the receiver, and the third one – the content. 
+On the final line, you will be given the indices of the sent emails separated by comma and space ", ". 
+Call the send() method for the given indices of emails. For each email, call the get_info() method.
 Note: submit all of your code, including the class
-_____________________________________________
+
 
 Example_01
 
-Input
-Peter
-John
-Katy
-
-Output
-End	Going: Peter, John, Katy
-Total: 3
-____________________________________________
-Example_02
 
 Input
-Sam
-Eddy
-Edd
-Kris
-End	
+Peter John Hi,John
+John Peter Hi,Peter!
+Katy Lilly Hello,Lilly
+Stop
+0, 2	
 
 Output
-Going: Sam, Eddy, Edd, Kris
-Total: 4
+Peter says to John: Hi,John. Sent: True
+John says to Peter: Hi,Peter!. Sent: False
+Katy says to Lilly: Hello,Lilly. Sent: True
+____________________________________________________
+Example_01
+
+
+Input
+Anna, Bella, Hi
+Sam, Dany, Okey
+Felix, Mery, Bye
+Stop
+0	
+
+Output
+Anna, says to Bella,: Hi. Sent: True
+Sam, says to Dany,: Okey. Sent: False
+Felix, says to Mery,: Bye. Sent: False
+
 
 '''
