434e916981cc4b1da13c707459d185a5c5faf882
Yanko Rashkov <thewhitedead@abv.bg>
2022-11-02 20:41:38 +0200

    Create 05ascii_values.py

---
+++ 24 Dictionaries - Lab/05ascii_values.py
@@ -0,0 +1,33 @@
+print({let: ord(let) for let in input().split(', ')})
+
+
+
+#################################### TASK CONDITION ############################
+"""
+                   5.	 ASCII Values
+Write a program that receives a list of characters separated by ", ". 
+It should create a dictionary with each character as a key and its ASCII value as a value. 
+
+Try solving that problem using comprehension.
+
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+a, b, c, a
+
+Output
+{'a': 97, 'b': 98, 'c': 99}
+
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+d, c, m, h
+
+Output
+{'d': 100, 'c': 99, 'm': 109, 'h': 104}
+
+"""
